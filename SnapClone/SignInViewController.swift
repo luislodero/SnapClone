50ff648637ac8aa6fd2384c64bb0890297c3d78c
Luis Lopez <luislodero@gmail.com>
2017-06-23 18:16:48 -0500

    CocoPods working, Firebase up and running, working on second View Controller

---
+++ SnapClone/SignInViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SignInViewController.swift
+//  SnapClone
+//
+//  Created by Luis Lopez on 6/22/17.
+//  Copyright © 2017 Luis Lopez. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func turnUpTapped(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("We tried to sign in")
+            if error != nil{
+                print("We have an error: \(String(describing: error))")
+                
+                Auth.auth().createUser(withEmail: self.emailTextField.text! , password: self.passwordTextField.text! , completion: { (user, error) in
+                    print("We tried to create an user")
+                    
+                    if error != nil {
+                        print("We have an error: \(String(describing: error))")
+
+                    }else{
+                        print("Created user successfully")
+                        self.performSegue(withIdentifier: "signinsegue", sender: nil)
+                    }
+                })
+            }else{
+                print("Signed in successfully")
+                self.performSegue(withIdentifier: "signinsegue", sender: nil)
+            }
+            
+        }
+        
+    }
+    
+    
+}
+
---
+++ SnapClone/SnapsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SnapsViewController.swift
+//  SnapClone
+//
+//  Created by Luis Lopez on 6/23/17.
+//  Copyright © 2017 Luis Lopez. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logOutTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+   
+}
