d1eeda33bb22e594fb2df59559f675bf0e30e933
Luis Lopez <luislodero@gmail.com>
2017-06-27 18:11:29 -0500

    UI Image Picker is now working

---
+++ SnapClone/PictureViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PictureViewController.swift
+//  SnapClone
+//
+//  Created by Luis Lopez on 6/23/17.
+//  Copyright © 2017 Luis Lopez. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imageView.backgroundColor = UIColor.clear
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func CameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func nextTapped(_ sender: Any) {
+    }
+    
+}
