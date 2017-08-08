16a182e1b33aac79aff70b5fd851ac4c51e2e430
Luis Lopez <luislodero@gmail.com>
2017-08-08 15:05:19 -0500

    FUCKING THING IS NOT WORKING! DONE WITH IT

---
+++ SnapClone/Snap.swift
@@ -0,0 +1,15 @@
+//
+//  Snap.swift
+//  SnapClone
+//
+//  Created by Luis Lopez on 8/8/17.
+//  Copyright © 2017 Luis Lopez. All rights reserved.
+//
+
+import Foundation
+
+class Snap {
+    var imageURL = ""
+    var descrip = ""
+    var from = ""
+}
--- SnapClone/SnapsViewController.swift
+++ SnapClone/SnapsViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import Firebase
 
 class SnapsViewController: UIViewController {
+    
+    var snaps : [Snap] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Database.database().reference().child("users").child(Auth.auth().currentUser!.uid).child("snaps").observe(DataEventType.childAdded, with: { (snapshot) in print(snapshot)
+            
+            let snap = Snap()
+            
+            if let userDictionary = snapshot.value as? NSDictionary{
+                
+                if case let snap??imageURL = userDictionary["imageURL"] as? String{
+            
+            snap.imageURL = snapshot.value!["imageURL"] as! String
+            snap.from = snapshot.value!["from"] as! String
+            snap.descrip = snapshot.value!["description"] as! String
+            
+            self.snaps.append(snap)
+                }
+            
+
+            
+//            let user = Snap()
+//            
+//            if let userDictionary = snapshot.value as? NSDictionary{
+//                
+//                if let email = userDictionary["email"] as? String{
+//                    
+//                    
+//                    user.email = email
+//                    user.uid = snapshot.key
+//                    
+//                    self.users.append(user)
+//                    
+//                    self.tableView.reloadData()
+                })
     }
+
+    
 
     @IBAction func logOutTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
